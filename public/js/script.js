0ac461c04fa5491c8582dbd1adc412aa469779b3
yosuaelw <cuatiawww@gmail.com>
2023-06-30 10:30:35 +0700

    update landing page

--- public/js/script.js
+++ public/js/script.js
@@ -3,6 +3,7 @@ const kategori = document.getElementById('kategori');
 const katalog = document.getElementById('katalog');
 const detailProduct = document.getElementById('proDetails');
 const serviceByJastip = document.getElementById('serviceByJastip');
+const brand = document.getElementById('brandLogo');
 // Carousel
 fetch('./carousel.html').then(function(snap){
     snap.text().then(function(result){
@@ -29,5 +30,30 @@ fetch('./serviceByJastip.html').then(function(snap){
         serviceByJastip.innerHTML = result;
     })
 })
+fetch('./brand.html').then(function(snap){
+    snap.text().then(function(result){
+        brand.innerHTML = result;
+    })
+})
+
+// Brand
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    });
+});
 
 
