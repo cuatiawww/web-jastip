1fdbb09c31166d3a7223dfc8c9cf658f38b73fdb
yosuaelw <cuatiawww@gmail.com>
2023-07-17 21:58:36 +0700

    sedang diperbaiki

--- public/js/randomkodeBrg.js
+++ public/js/randomkodeBrg.js
@@ -1,19 +1,19 @@
-// kodeBrgBook berfungsi menghasilkan 4 angka random yang akan di simpan kedalam local storage lalu ditangkap dengan tag html yang memiliki id "kodeBrg"
-function kodeBrgBook() {
-    let kodeBrgElements = document.getElementsByClassName("kodeBrg");
+// // kodeBrgBook berfungsi menghasilkan 4 angka random yang akan di simpan kedalam local storage lalu ditangkap dengan tag html yang memiliki id "kodeBrg"
+// function kodeBrgBook() {
+//     let kodeBrgElements = document.getElementsByClassName("kodeBrg");
   
-    for (let i = 0; i < kodeBrgElements.length; i++) {
-      let element = kodeBrgElements[i];
-      let index = element.getAttribute("data-index");
-      // Membaca nilai kode barang dari localStorage berdasarkan kombinasi "data-index" dan indeks elemen
-      let kodeBarang = localStorage.getItem(index + "_" + i);
-      // angka acak
-      if (!kodeBarang) {
-        kodeBarang = Math.floor(Math.random() * 9999) + 1;
-        localStorage.setItem(index + "_" + i, kodeBarang);
-      }
+//     for (let i = 0; i < kodeBrgElements.length; i++) {
+//       let element = kodeBrgElements[i];
+//       let index = element.getAttribute("data-index");
+//       // Membaca nilai kode barang dari localStorage berdasarkan kombinasi "data-index" dan indeks elemen
+//       let kodeBarang = localStorage.getItem(index + "_" + i);
+//       // angka acak
+//       if (!kodeBarang) {
+//         kodeBarang = Math.floor(Math.random() * 9999) + 1;
+//         localStorage.setItem(index + "_" + i, kodeBarang);
+//       }
   
-      element.innerHTML = kodeBarang;
-    }
-}
-document.addEventListener("DOMContentLoaded", kodeBrgBook);
+//       element.innerHTML = kodeBarang;
+//     }
+// }
+// document.addEventListener("DOMContentLoaded", kodeBrgBook);
