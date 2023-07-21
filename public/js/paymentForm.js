982bb919f3ec694f45d4cbf0c0f7eae7c1d4e805
yosuaelw <cuatiawww@gmail.com>
2023-07-21 12:37:43 +0700

    payment

--- public/js/paymentForm.js
+++ public/js/paymentForm.js
@@ -7,7 +7,7 @@ function sendWhatsAppMessage() {
     let city = document.getElementsByName('city')[0].value;
     let state = document.getElementsByName('state')[0].value;
     let zipcode = document.getElementsByName('zipcode')[0].value;
-    let kodeBrg = document.getElementsByName('kodeBarang')[0].value;
+    let itemName = document.getElementsByName('itemName')[0].value;
     let cardname = document.getElementsByName('cardname')[0].value;
     let cardnumber = document.getElementsByName('cardnumber')[0].value;
     let expmonth = document.getElementsByName('expmonth')[0].value;
@@ -16,7 +16,7 @@ function sendWhatsAppMessage() {
     // URL WhatsApp
     let whatsappUrl = "https://api.whatsapp.com/send?phone=6281908775565&text=";
     // Data yang akan dikirimkan ke url WhatsApp
-    let message = `Username: ${username}\r\n\r\nEmail: ${email}\r\n\r\nAddress: ${address}\r\n\r\nCity: ${city}\r\n\r\nState: ${state}\r\n\r\nZip Code: ${zipcode}\r\n\r\nItem code: ${kodeBrg}\r\n\r\nName on Card: ${cardname}\r\n\r\nCredit Card Number: ${cardnumber}\r\n\r\nExp Month: ${expmonth}\r\n\r\nExp Year: ${expyear}\r\n\r\nCVV: ${cvv}`;
+    let message = `Username: ${username}\r\n\r\nEmail: ${email}\r\n\r\nAddress: ${address}\r\n\r\nCity: ${city}\r\n\r\nState: ${state}\r\n\r\nZip Code: ${zipcode}\r\n\r\nItem name: ${itemName}\r\n\r\nName on Card: ${cardname}\r\n\r\nCredit Card Number: ${cardnumber}\r\n\r\nExp Month: ${expmonth}\r\n\r\nExp Year: ${expyear}\r\n\r\nCVV: ${cvv}`;
 
     // Membuka whatsapp url dan cek kode unik
     window.open(whatsappUrl + encodeURIComponent(message));
